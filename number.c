299c0194ba88f0c7fe7112c2e11d743fb332c09b
git893dharshinikt <84172487+git893dharshinikt@users.noreply.github.com>
2021-06-19 19:29:21 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+{
+	int choice=3;
+	switch(choice)
+	{
+		case 0:
+			printf("zero is a valid");
+			break;
+		case 1:
+			printf("one is a valid");
+			break;
+		default:
+			printf("invalid number");
+	 }
+}
